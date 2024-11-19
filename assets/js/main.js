e1e4d690a676c3e48b2e818d590d6b0e56d54a8e
hamada <bhak6150@gmail.com>
2024-11-19 21:50:39 +0100

    Scroll top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+  /**
+   *  Start Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+/*  End Scroll top button  */ 
+
+
+
+
+
+
 // افترض أن لدينا قائمة عناصر في النافبار مع فئة "nav-item"
 const navLinks = document.querySelectorAll('.nav-item a');
 
@@ -10,6 +38,5 @@ navLinks.forEach(link => {
     link.classList.add('active');
   }
 });
-
 
 
