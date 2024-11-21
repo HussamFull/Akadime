5546844841038261aa0b2beaeb2686a7ee6621b7
hamada <bhak6150@gmail.com>
2024-11-21 20:05:40 +0100

    navlinks-update

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,3 @@
 
 
 
-
-// افترض أن لدينا قائمة عناصر في النافبار مع فئة "nav-item"
-const navLinks = document.querySelectorAll('.nav-item a');
-
-// الحصول على عنوان URL الحالي
-const currentUrl = window.location.href;
-
-// مقارنة كل عنصر في القائمة مع عنوان URL الحالي وإضافة الكلاس النشط
-navLinks.forEach(link => {
-  if (link.href === currentUrl) {
-    link.classList.add('active');
-  }
-});
-
-
